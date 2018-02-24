e8af2a1e133d17dde5d2dbf4278eda51a698d387
Robin Smith <robin@rsmith.io>
2018-02-24 17:28:53 +0100

    Quick-save.

--- practice/destutter_list.ml
+++ practice/destutter_list.ml
@@ -21,21 +21,55 @@ let _ =
         | first :: second :: rest -> if first = second then (destutter_list (first :: rest)) else (first :: destutter_list (second :: rest))
     in
 
-    let test list expected =
+    let destutter_list_sean list =
+        let rec _destutter_list list previous =
+            match list with
+            | [] -> list
+            | h::t -> 
+                let replacement =
+                match previous with
+                | None -> [h]
+                | Some previous_item -> if h = previous_item then [] else [h]
+                in
+                    replacement @ _destutter_list t (Some h) ;
+        in
+        _destutter_list list None ;
+    in
+    
+    let test_destutter destutterer list expected =
+        Printf.printf "list: " ;
+        pretty_prent_list list ;
         Printf.printf "expected: " ;
         pretty_prent_list expected ;
-        let actual = destutter_list list in
+        let actual = destutterer list in
         Printf.printf "actual: " ;
         pretty_prent_list actual ;
         let res = (expected = actual) in
         if res then Printf.printf "PASS\n" else Printf.printf "TEST ASSERTION ERROR\n" ;
         assert(res)
     in
+
+    let test_sean list expected =
+        test_destutter destutter_list_sean list expected
+    in
+
+    let test_robin list expected = 
+        test_destutter destutter_list list expected
+    in
+
+    let test list expected =
+        Printf.printf "TESTING ROBIN\n" ;
+        test_robin list expected ;
+
+        Printf.printf "TESTING SEAN\n" ;
+        test_sean list expected ;
+    in
         
     test [1] [1] ;
     test [1; 1] [1] ;
     test [1; 1; 1] [1] ;
     test [1; 2; 3] [1; 2; 3] ;
+    test [1; 1; 2; 2; 1; 1] [1; 2; 1] ;
     test 
         [1; 1; 2; 3; 4; 4; 5; 5; 5; 6; 7; 8; 8; 9]
         [1; 2; 3; 4; 5; 6; 7; 8; 9;]
