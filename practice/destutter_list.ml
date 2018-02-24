9fe5a44d6f3e05daf0abc3780ec44bdbeea9eebe
Robin Smith <robin@rsmith.io>
2018-02-24 17:56:34 +0100

    Quick-save.

--- practice/destutter_list.ml
+++ practice/destutter_list.ml
@@ -1,8 +1,20 @@
 #!/usr/bin/env ocaml
 
 let _ =
-    let pretty_prent_list list =
+    let pretty_prent_list_int list =
         let print_int x = Printf.printf " %d;" x in
+
+        let rec print_list_d = function
+        | [] -> ()
+        | e::l -> print_int e ; print_list_d l in
+
+        Printf.printf "[" ;
+        print_list_d list ;
+        Printf.printf " ]\n" ;
+    in
+
+    let pretty_prent_list_str list =
+        let print_int x = Printf.printf " %s;" x in
 
         let rec print_list_d = function
         | [] -> ()
@@ -18,64 +30,76 @@ let _ =
         | [] -> list
         | first :: [] -> list
         | first :: second :: [] -> first :: (if first = second then [] else [second] )
-        | first :: second :: rest -> if first = second then (destutter_list (first :: rest)) else (first :: destutter_list (second :: rest))
+        | first :: second :: rest -> if first = second
+        then (destutter_list (first :: rest))
+        else (first :: destutter_list (second :: rest))
     in
 
     let destutter_list_sean list =
         let rec _destutter_list list previous =
             match list with
             | [] -> list
-            | h::t -> 
-                let replacement =
-                match previous with
-                | None -> [h]
-                | Some previous_item -> if h = previous_item then [] else [h]
-                in
-                    replacement @ _destutter_list t (Some h) ;
+            | head :: rest -> 
+            let current = [head] in
+            let replacement = if current = previous then [] else [head] in 
+            replacement @ _destutter_list rest current ;
         in
-        _destutter_list list None ;
+        _destutter_list list [] ;
     in
     
-    let test_destutter destutterer list expected =
+    let test_destutter printer destutterer list expected =
         Printf.printf "list: " ;
-        pretty_prent_list list ;
+        printer list ;
         Printf.printf "expected: " ;
-        pretty_prent_list expected ;
+        printer expected ;
         let actual = destutterer list in
         Printf.printf "actual: " ;
-        pretty_prent_list actual ;
+        printer actual ;
         let res = (expected = actual) in
         if res then Printf.printf "PASS\n" else Printf.printf "TEST ASSERTION ERROR\n" ;
         assert(res)
     in
 
-    let test_sean list expected =
-        test_destutter destutter_list_sean list expected
+    let test_sean tester list expected =
+        tester destutter_list_sean list expected ;
     in
 
-    let test_robin list expected = 
-        test_destutter destutter_list list expected
+    let test_robin tester list expected = 
+        tester destutter_list list expected ;
     in
 
-    let test list expected =
-        Printf.printf "TESTING ROBIN\n" ;
-        test_robin list expected ;
+    let test printer list expected =
+        let tester = test_destutter printer in
+            Printf.printf "TESTING ROBIN\n" ;
+            test_robin tester list expected ;
 
-        Printf.printf "TESTING SEAN\n" ;
-        test_sean list expected ;
+            Printf.printf "TESTING SEAN\n" ;
+            test_sean tester list expected ;
     in
-        
-    test [1] [1] ;
-    test [1; 1] [1] ;
-    test [1; 1; 1] [1] ;
-    test [1; 2; 3] [1; 2; 3] ;
-    test [1; 1; 2; 2; 1; 1] [1; 2; 1] ;
-    test 
+
+    let test_int list expected =
+        test pretty_prent_list_int list expected ;
+    in
+
+    let test_str list expected =
+        test pretty_prent_list_str list expected ;
+    in
+
+    test_int [1] [1] ;
+    test_int [1; 1] [1] ;
+    test_int [1; 1; 1] [1] ;
+    test_int [1; 2; 3] [1; 2; 3] ;
+    test_int [1; 1; 2; 2; 1; 1] [1; 2; 1] ;
+    test_int 
         [1; 1; 2; 3; 4; 4; 5; 5; 5; 6; 7; 8; 8; 9]
         [1; 2; 3; 4; 5; 6; 7; 8; 9;]
+    ;
+    test_str
+        ["Robin"; "Robin"; "Andrew"; "Smith"; "Smith"; "Smith"]
+        ["Robin"; "Andrew"; "Smith"]
     ;
 
     Printf.printf "\n" ;
     let list = [1; 1; 2; 3; 4; 4; 5; 5; 5; 6; 7; 8; 8; 9] in
-        pretty_prent_list (destutter_list list);
+        pretty_prent_list_int (destutter_list list);
 
