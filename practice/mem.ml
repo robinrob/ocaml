faf8d4b065d148a8bb7dbe7e89dbc45aa4ed6ff7
Robin Smith <robin@rsmith.io>
2018-02-25 18:15:40 +0000

    Quick-save.

--- practice/mem.ml
+++ practice/mem.ml
@@ -1,9 +1,5 @@
 #!/usr/bin/env ocaml
 
-
 let _ = 
     let in_mem = (List.mem 1 [1;2;3]) in
     Printf.printf "in_mem: %b\n" in_mem ;
-
-    let exists = (List.exists (fun el -> el = 1) [1;2;3]) in
-    Printf.printf "exists: %b\n" exists
--- practice/optionals.ml
+++ practice/optionals.ml
@@ -1,9 +1,11 @@
 #!/usr/bin/env ocaml
 
-(* let _ = 
+let _ = 
     let divide x y =
-        if y = 0 then None else Some (x/y) ;
+        if y = 0. then None else Some (x /. y) ;
     in
        
-    let ans = divide 2 3 ;
-    Printf.printf "ans: %d\n" ans ; *)
+    let ans = divide 2. 3. in
+        match ans with
+        | Some ans -> Printf.printf "ans: %.2f\n" ans 
+        | None -> Printf.printf "ans: None" 
