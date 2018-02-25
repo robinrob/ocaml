a787517695ca8c26ab4d220896b172df459122cb
Robin Smith <robin@rsmith.io>
2018-02-25 18:21:54 +0000

    1.0.1
    Do lots of improvements and tidying.

--- practice/is_in_list.ml
+++ practice/is_in_list.ml
@@ -1,9 +1,17 @@
 #!/usr/bin/env ocaml
 
+(* #use "topfind"
+#thread
+#require "core"
+open Core *)
 
 let _ = 
     let in_mem = (List.mem 1 [1;2;3]) in
     Printf.printf "in_mem: %b\n" in_mem ;
+
+    (* Use this ordering when using Core module ... *)
+    (* let exists = (List.exists [1;2;3] (fun el -> el = 1)) in
+    Printf.printf "exists: %b\n" exists *)
 
     let exists = (List.exists (fun el -> el = 1) [1;2;3]) in
     Printf.printf "exists: %b\n" exists
